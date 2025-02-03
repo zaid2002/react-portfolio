599b4b2c6e5281707dd94f574660bcf80e21e32e
Zaid Shariff <zaidshamsahmed@gmail.com>
2025-02-04 00:47:47 +0530

    Update Navbar.jsx
    Added links to the navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,10 @@ const Navbar = () => {
                     <img className="mx-2 w-10" src={logo} alt="logo" />
                 </div>
                 <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
-                    <FaLinkedin/>
-                    <FaGithub/>
-                    <FaInstagram/>
+                    <a href="https://www.linkedin.com/in/zaid-shariff-6354902b9/"><FaLinkedin/></a>
+                    <a href="https://github.com/zaid2002"><FaGithub/></a>
+                    <a href="https://www.instagram.com/md_zaid_22_/?hl=en"><FaInstagram/></a>
+                    
                 </div>
             </nav>   
     )
